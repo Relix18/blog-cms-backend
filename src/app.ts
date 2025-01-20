924a42d8dbb26ee0d2c252b97316f6cab166b88a
Relix18 <relix049@gmail.com>
2025-01-20 15:00:45 +0530

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ export const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: " http://localhost:3000 ", credentials: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
