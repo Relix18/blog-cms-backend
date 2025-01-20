9d75d761bd7ebd5db37730522496ba075d2915b4
Relix18 <relix049@gmail.com>
2025-01-20 16:45:06 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ export const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: "https://orbitblog.vercel.app" }));
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
