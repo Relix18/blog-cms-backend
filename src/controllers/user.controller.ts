d8385890feb9cba430fef8ee860f1bf69ed623c4
Relix18 <relix049@gmail.com>
2025-01-21 13:59:39 +0530

    logout fix

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -262,6 +262,13 @@ export const logout = TryCatch(
   async (req: Request, res: Response, next: NextFunction) => {
     res.clearCookie("access_token");
 
+    res.cookie("access_token", "", {
+      expires: new Date(0),
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
     res.status(200).json({
       success: true,
       message: "Logout Successfully",
